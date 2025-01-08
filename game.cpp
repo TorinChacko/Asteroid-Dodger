c73b0ae36c87708cea9d463512ff2b155de2b7a6
Torin Chacko <chackotorin@gmail.com>
2025-01-08 15:53:45 -0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -10,12 +10,17 @@ using namespace std;
 bool gameOver;
 const int width = 40;
 const int height = 7;
-int x = 20, y = 6, fruitX[10], fruitY[10], carX, carY;
+int playerX = 20, playerY = 6, obstacleX[10], obstacleY[10];
+int logX[10], logY;
 
 void Setup() {
     gameOver = false;
-    carX = rand() % width;
-    carY = 0;
+    obstacleX[1] = rand() % width;
+    obstacleY[1] = 0;
+    logY = 3;
+    for (int i = 0; i < 10; i++) {
+        logX[i] = rand() % width;
+    }
 }
 
 void Map() {
@@ -26,36 +31,36 @@ void Map() {
     }
     for (int i = 0; i < height; i++) {
         for (int j = 0; j <= width; j++) {
-            if (i == y && j == x) {
-                if (y == 1) gameOver = true;
+            if (i == playerY && j == playerX) {
+                if (playerY == 1) gameOver = true;
                 cout << "$";
             }
-            else if (i == carY && j == carX) {
-                cout << "C";
+            else if (i == obstacleY[1] && j == obstacleX[1]) {
+                cout << "O";
             }
-            else if (i == fruitY[1] && j == fruitX[1]) {
-                cout << "#";
+            else if (i == logY && (j == logX[0] || j == logX[1] || j == logX[2] || j == logX[3] || j == logX[4] || j == logX[5] || j == logX[6] || j == logX[7] || j == logX[8] || j == logX[9])) {
+                cout << "_";
             }
-            else if (i == fruitY[2] && j == fruitX[2]) {
-                cout << "#";
+            else if (i == obstacleY[2] && j == obstacleX[2]) {
+                cout << "O";
             }
-            else if (i == fruitY[3] && j == fruitX[3]) {
-                cout << "#";
+            else if (i == obstacleY[3] && j == obstacleX[3]) {
+                cout << "O";
             }
-            else if (i == fruitY[4] && j == fruitX[4]) {
-                cout << "#";
+            else if (i == obstacleY[4] && j == obstacleX[4]) {
+                cout << "O";
             }
-            else if (i == fruitY[5] && j == fruitX[5]) {
-                cout << "#";
+            else if (i == obstacleY[5] && j == obstacleX[5]) {
+                cout << "O";
             }
-            else if (i == fruitY[6] && j == fruitX[6]) {
-                cout << "#";
+            else if (i == obstacleY[6] && j == obstacleX[6]) {
+                cout << "O";
             }
-            else if (i == fruitY[7] && j == fruitX[7]) {
-                cout << "#";
+            else if (i == obstacleY[7] && j == obstacleX[7]) {
+                cout << "O";
             }
-            else if (i == fruitY[8] && j == fruitX[8]) {
-                cout << "#";
+            else if (i == obstacleY[8] && j == obstacleX[8]) {
+                cout << "O";
             }
             else {
                 cout << " ";
@@ -74,16 +79,16 @@ void Input() {
     if (_kbhit()) {
         switch (_getch()) {
         case 'a':
-            x--;
+            playerX--;
             break;
         case 'd':
-            x++;
+            playerX++;
             break;
         case 'w':
-            y--;
+            playerY--;
             break;
         case 's':
-            y++;
+            playerY++;
             break;
         case 'x':
             gameOver = true;
@@ -93,105 +98,117 @@ void Input() {
 }
 
 void Logic() {
-    if (x > width || x < 0 || y > height || y < 0)
+    if (playerX > width || playerX < 0 || playerY > height || playerY < 0)
         gameOver = true;
     
-    carY++;
-    if (carY >= height) {
-        carY = 0;
-        carX = rand() % width;
+    obstacleY[1]++;
+    if (obstacleY[1] >= height) {
+        obstacleY[1] = 0;
+        obstacleX[1] = rand() % width;
     }
-    if (x == carX && y == carY) {
+    if (playerX == obstacleX[1] && playerY == obstacleY[1]) {
         gameOver = true;
     }
 
-    fruitX[1]++;
-    fruitX[2]++;
-    fruitX[3]++;
-    fruitX[4]++;
-    fruitX[5]++;
-    fruitX[6]++;
-    fruitX[7]++;
-    fruitX[8]++;
+    for (int i = 0; i < 10; i++) {
+        logX[i]++;
+        if (logX[i] >= width) {
+            logX[i] = 0;
+        }
+    }
 
-    if (x == fruitX[1] && y == fruitY[1]) {
+    if (playerY == logY) {
+        bool onLog = false;
+        for (int i = 0; i < 10; i++) {
+            if (playerX == logX[i]) {
+                onLog = true;
+                break;
+            }
+        }
+        if (!onLog) {
+            gameOver = true;
+        }
+    }
+
+    obstacleX[2]++;
+    obstacleX[3]++;
+    obstacleX[4]++;
+    obstacleX[5]++;
+    obstacleX[6]++;
+    obstacleX[7]++;
+    obstacleX[8]++;
+
+    if (playerX == obstacleX[2] && playerY == obstacleY[2]) {
         gameOver = true;
     }
-    if (x == fruitX[2] && y == fruitY[2]) {
+    if (playerX == obstacleX[3] && playerY == obstacleY[3]) {
         gameOver = true;
     }
-    if (x == fruitX[3] && y == fruitY[3]) {
+    if (playerX == obstacleX[4] && playerY == obstacleY[4]) {
         gameOver = true;
     }
-    if (x == fruitX[4] && y == fruitY[4]) {
+    if (playerX == obstacleX[5] && playerY == obstacleY[5]) {
         gameOver = true;
     }
-    if (x == fruitX[5] && y == fruitY[5]) {
+    if (playerX == obstacleX[6] && playerY == obstacleY[6]) {
         gameOver = true;
     }
-    if (x == fruitX[6] && y == fruitY[6]) {
+    if (playerX == obstacleX[7] && playerY == obstacleY[7]) {
         gameOver = true;
     }
-    if (x == fruitX[7] && y == fruitY[7]) {
+    if (playerX == obstacleX[8] && playerY == obstacleY[8]) {
         gameOver = true;
     }
-    if (x == fruitX[8] && y == fruitY[8]) {
-        gameOver = true;
+    if (obstacleX[2] == 39) {
+        obstacleX[2] = 1;
+        obstacleY[2] = 5;
     }
-    if (fruitX[1] == 39) {
-        fruitX[1] = 1;
-        fruitY[1] = 5;
+    if (obstacleX[3] == 39) {
+        obstacleX[3] = 1;
+        obstacleY[3] = 5;
     }
-    if (fruitX[2] == 39) {
-        fruitX[2] = 1;
-        fruitY[2] = 5;
+    if (obstacleX[4] == 39) {
+        obstacleX[4] = 1;
+        obstacleY[4] = 4;
     }
-    if (fruitX[3] == 39) {
-        fruitX[3] = 1;
-        fruitY[3] = 4;
+    if (obstacleX[5] == 39) {
+        obstacleX[5] = 1;
+        obstacleY[5] = 4;
     }
-    if (fruitX[4] == 39) {
-        fruitX[4] = 1;
-        fruitY[4] = 4;
+    if (obstacleX[6] == 39) {
+        obstacleX[6] = 1;
+        obstacleY[6] = 3;
     }
-    if (fruitX[5] == 39) {
-        fruitX[5] = 1;
-        fruitY[5] = 3;
+    if (obstacleX[7] == 39) {
+        obstacleX[7] = 1;
+        obstacleY[7] = 2;
     }
-    if (fruitX[6] == 39) {
-        fruitX[6] = 1;
-        fruitY[6] = 3;
-    }
-    if (fruitX[7] == 39) {
-        fruitX[7] = 1;
-        fruitY[7] = 2;
-    }
-    if (fruitX[8] == 39) {
-        fruitX[8] = 1;
-        fruitY[8] = 2;
+    if (obstacleX[8] == 39) {
+        obstacleX[8] = 1;
+        obstacleY[8] = 2;
     }
 }
 
 int main() {
-    fruitX[1] = 1;
-    fruitY[1] = 5;
-    fruitX[2] = 3;
-    fruitY[2] = 5;
+    obstacleX[1] = 1;
+    obstacleY[1] = 5;
+    obstacleX[2] = 3;
+    obstacleY[2] = 5;
 
-    fruitX[3] = 11;
-    fruitY[3] = 4;
-    fruitX[4] = 18;
-    fruitY[4] = 4;
+    obstacleX[3] = 11;
+    obstacleY[3] = 4;
+    obstacleX[4] = 18;
+    obstacleY[4] = 4;
 
-    fruitX[5] = 25;
-    fruitY[5] = 3;
-    fruitX[6] = 32;
-    fruitY[6] = 3;
+    obstacleX[5] = 25;
+    obstacleY[5] = 3;
+    obstacleX[6] = 32;
+    obstacleY[6] = 3;
 
-    fruitX[7] = 1;
-    fruitY[7] = 2;
-    fruitX[8] = 25;
-    fruitY[8] = 2;
+    obstacleX[7] = 1;
+    obstacleY[7] = 2;
+    obstacleX[8] = 25;
+    obstacleY[8] = 2;
     Setup();
     while (!gameOver) {
         Map();
